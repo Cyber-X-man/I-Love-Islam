7ed934dc18749471ca013cfe0021212d554f72f6
Cyber-X-man <121112632+Cyber-X-man@users.noreply.github.com>
2023-01-23 20:16:58 +0600

    Add files via upload

---
+++ allow_islam.py
@@ -0,0 +1,22 @@
+print("""•••••••••••••••••••••••••••••••••••••••••••
+•     _    _          ____ _____  ___     •
+•    | |  | |   /\   |  _ \_   _|  _ \    •
+•    | |__| |  /  \  | |_) || | | |_) |   •
+•    |  __  | / /\ \ |  _ < | | |  _ <    •
+•    | |  | |/ ____ \| |_) || |_| |_) |   •
+•    |_|  |_/_/    \_\____/_____|____/    •
+•                                         •
+•      [I AM PROUD THAT I AM MUSLIM]      •
+•                                         •
+•••••••••••••••••••••••••••••••••••••••••••
+                                   
+""")
+name = input("Enter Your Name : ")
+religion = input("Enter Your Religion : ")
+def num():
+  if religion == "Islam":
+    return True
+  else:
+    return False
+    
+print(num())
